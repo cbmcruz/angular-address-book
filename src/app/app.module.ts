9c5ca1cdae0867574770f0ba320dd53fe7992308
cbmcruz <cbmcruz.31@gmail.com>
2021-03-10 12:23:38 -0700

    Added ngbootstrap, generated components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { ContactSearchComponent } from './contact-search/contact-search.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactDetailComponent,
+    ContactSearchComponent,
+    ContactsComponent,
+    FavoritesComponent,
+    MessagesComponent,
+    ContactEditComponent,
+    ContactCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
